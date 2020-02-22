1711a2a0e31d831e40137203c9abcba0bf56fcad
Jamesweng <cnshoit@gmail.com>
2020-02-22 23:39:52 +0800

    Update 0092-reverse-linked-list-ii.cpp
    Add comments

--- 0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii.cpp
@@ -20,8 +20,10 @@ public:
         n++;
         
         ListNode* p1 = head, *p2 = head->next, *px, *py;
-        
-        for (int j = 2; p2 != NULL; j++) {
+        // 1->2->..->m-1->m->m+1->...->n-1->n->n+1->...
+        // 1->2->..->m-1->n->n-1->...->m+1->m->n+1->...
+        // px: node m-1, py: node m, p1: prev node, p2: current node
+        for (int j = 2; p2 != NULL && j <= n; j++) {
             if (m < j && j <= n) {
                 if (j == n) {
                     px->next = p2;
